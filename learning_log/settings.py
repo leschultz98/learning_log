4ae2ecca335120193c2ed29543570cb706b3e219
schultz <ambassador.life@gmail.com>
2020-06-12 20:47:14 +0700

    edit heroku setting

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -130,3 +127,6 @@ LOGIN_URL = '/users/login/'
 BOOTSTRAP3 = {
     'include_jquery': True,
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
